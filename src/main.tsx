90571518f07bf0f0cc2ad1804b837203bd040ce6
thanhlv <thanh.3ac.vn@gmaill.com>
2026-07-21 23:57:47 +0700

    fix bug 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "../index";
 import "./index.css";
 import { LanguageProvider } from "./i18n/LanguageContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
 
